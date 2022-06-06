407a612edc2b6166e134c5de475278cf9fa4bd4e
Zachary Goncalves <zach@macbook-pro.mynetworksettings.com>
2022-06-05 22:32:59 -0400

    scaffold part of site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Jumbotron from './components/Jumbotron/Jumbotron.js';
+import Header from './components/Header/Header.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+        <Header />
+        <Jumbotron tagline="Hi, I’m Zach. I’m a full-stack software developer from Philadelphia currently working at Vertex Inc."/>
+      </div>
     </div>
   );
 }
