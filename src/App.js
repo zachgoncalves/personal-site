c0b05049b2a0e266fde32af4e5c798e6737ac70e
Zach Goncalves <zach.j.goncalves@gmail.com>
2023-08-05 16:14:21 -0400

    add experience scaffolding

--- src/App.js
+++ src/App.js
@@ -2,19 +2,52 @@ import logo from './logo.svg';
 import './App.scss';
 import Jumbotron from './components/Jumbotron/Jumbotron.js';
 import Header from './components/Header/Header.js';
-import Experience from './components/Experience/Experience.js'
+import TechnicalSkills from './components/TechnicalSkills/TechnicalSkills.js'
 import Footer from './components/Footer/Footer.js'
+import Experience from './components/Experience/Experience';
 
 function App() {
   return (
     <div className="App">
-      <div class="container">
+      <div className="container">
         <Header />
         <Jumbotron tagline="Hi, I’m Zach. I’m a full-stack software developer from Philadelphia currently working at Vertex Inc."/>
       </div>
 
-      <div class="justify-content-between">
-        <Experience />
+      <div className="justify-content-between">
+        <TechnicalSkills />
+      </div>
+      <div className="accordion" id="accordionExample">
+        <Experience 
+          job_title="Software Developer II"
+          job_length="November 2021 - Present"
+          job_location="King of Prussia, PA"
+          company="Vertex, Inc"
+        />
+        <Experience 
+          job_title="Software Developer I"
+          job_length="April 2021 - November 2021"
+          job_location="King of Prussia, PA"
+          company="Vertex, Inc"
+        />
+        <Experience 
+          job_title="Vertex Advance Program - Technical Track"
+          job_length="August 2020 – April 2021"
+          job_location="King of Prussia, PA"
+          company="Vertex, Inc"
+        />
+        <Experience 
+          job_title="Junior Web Developer"
+          job_length="May 2019 – August 2020"
+          job_location="Philadelphia, PA"
+          company="Temple University, Fox School of Business"
+        />
+        <Experience 
+          job_title="Creative Director + Partner"
+          job_length="June 2017 – September 2019"
+          job_location="Bensalem, PA"
+          company="Rubiar, LLC"
+        />
       </div>
       <Footer />
     </div>
--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,46 +1,26 @@
 import styles from './Experience.module.scss'
 
 const Experience = props => {
-    return (
-        <section className={styles.experience_section}>
-            <h3>I have experience in...</h3>
-            <ul className={styles.experience_list}>
-                <li><span>Microservices Development using</span>
-                    <ul>
-                        <li>Java</li>
-                        <li>Spring Boot</li>
-                        <li>OpenAPI</li>
-                        <li>PostgreSQL</li>
+    return (        
+        <div className="accordion-item">
+            <h2 className="accordion-header" id="headingOne">
+                <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    <h2>{props.job_title}</h2>
+                    <p>{props.company} / {props.job_location} / {props.job_length}</p>
+                </button>
+            </h2>
+            <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div className="accordion-body">
+                    <ul className={styles.experience_list}>
+                        <li>Design and implement data migration strategy and code for client adoption of a new product. Lead several junior developers on implementation across several services and two teams.</li>
+                        <li>Orchestrate Postgres 10.x database migration to Postgres 13.x on Aurora RDS. Validated application performance and stability, executed dry runs, created a run book, and successfully deployed.</li>
+                        <li>Deploy a new environment for testing monthly data updates in conjunction with our Research Data team, creating a flow that supported them in their validations prior to release of data updates to production. </li>
+                        <li>Decouple core product’s build process from calls to production services, substituting for mocks and contract driven testing, creating a GitHub Actions workflow, and extending a test framework (pyTest). </li>
                     </ul>
-                </li>
-                <li><span>Building and deploying code with</span>
-                    <ul>
-                        <li>Gradle</li>
-                        <li>Maven</li>
-                        <li>Jenkins</li>
-                        <li>Github Actions</li>
-                    </ul>
-                </li>
-                <li><span>Managing infrastructure with</span>
-                    <ul>
-                        <li>Terraform</li>
-                        <li>Terragrunt</li>
-                    </ul>
-                </li>
-                <li><span>Deploying on</span>
-                    <ul>
-                        <li>AWS</li>
-                    </ul>
-                </li>
-                <li><span>I also have worked with</span>
-                    <ul>
-                        <li>C# / ASP.NET</li>
-                        <li>JavaScript / React and Vue</li>
-                    </ul>
-                </li>
-            </ul>
-        </section>
+                </div>
+            </div>
+        </div>
     );
 };
 
-export default Experience;
+export default Experience; 
