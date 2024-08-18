6611f5d40eaa09358fb2534f73931f47e8c1fd42
zachgoncalves <zach.j.goncalves@gmail.com>
2024-08-17 23:32:47 -0400

    update experience

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -7,17 +7,19 @@ const Experience = props => {
             <ul className={styles.experience_list}>
                 <li><span>Microservices Development using</span>
                     <ul>
+                        <li>Go</li>
                         <li>Java</li>
                         <li>Spring Boot</li>
                         <li>OpenAPI</li>
                         <li>PostgreSQL</li>
                     </ul>
                 </li>
-                <li><span>Building and deploying code with</span>
+                <li><span>Building and deploying code and artifacts with</span>
                     <ul>
                         <li>Gradle</li>
                         <li>Maven</li>
                         <li>Jenkins</li>
+                        <li>ArgoCD</li>
                         <li>Github Actions</li>
                     </ul>
                 </li>
@@ -30,6 +32,8 @@ const Experience = props => {
                 <li><span>Deploying on</span>
                     <ul>
                         <li>AWS</li>
+                        <li>Azure</li>
+                        <li>Multi-cloud Kubernetes</li>
                     </ul>
                 </li>
                 <li><span>I also have worked with</span>
